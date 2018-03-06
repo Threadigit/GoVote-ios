97ece291df4f138f890f94ad13884b59a36ae75e
Adetuyi Tolu <tolu.adetuyi@gmail.com>
2018-03-07 00:23:38 +0100

    update location model

--- govote/Location.swift
+++ govote/Location.swift
@@ -12,23 +12,28 @@ class Location{
     
     var id: Int = 0
     var name:String = ""
-     var area: String = "Lagos"
+    var area: String = "Lagos"
+    var address: String = ""
     //var imgURL:String = "https://tech-wheel.herokuapp.com/images/techwheel_logo.png"
     var imgURL: String = ""
-    var address: String = ""
-   
+    
     var latitude: String = ""
     var longitude: String = ""
     var confirmed: String = ""
     var state: String = ""
     var city: String = ""
     
-    init(id:Int, name:String, area:String) {
+    init(id:Int, name:String, area:String, address:String) {
         
         self.id = id
         self.name = name
         self.area = area
-        self.imgURL = "https://maps.googleapis.com/maps/api/staticmap?center=\(area),+Nigeria&zoom=13&scale=1&size=100x100&maptype=roadmap&format=png&visual_refresh=true"
+        self.address = address
+        let add = address.replacingOccurrences(of: " ", with: "")
+        self.imgURL = "https://maps.googleapis.com/maps/api/staticmap?center=\(add)&zoom=13&scale=1&size=100x100&maptype=roadmap&format=png&visual_refresh=true"
+        print(imgURL)
     }
     
 }
+
+
