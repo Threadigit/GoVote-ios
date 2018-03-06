9eba47b8acc7403990174e8e7de08482e00ad1c0
Adetuyi Tolu <tolu.adetuyi@gmail.com>
2018-03-07 00:25:12 +0100

    add welcome controller

---
+++ govote/WelcomeController.swift
@@ -0,0 +1,23 @@
+//
+//  WelcomeController.swift
+//  govote
+//
+//  Created by Adetuyi Tolu Emmanuel on 3/6/18.
+//  Copyright © 2018 Adetuyi Tolu Emmanuel. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+
+}
+
+
+
+
